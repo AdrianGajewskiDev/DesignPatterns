2e8e712838f92618717b75166539d6cdab413b11
adrian.gajewskiDev@outlook.com <adrian.gajewskiDev@outlook.com>
2021-04-28 18:06:33 +0200

    add factory design pattern

---
+++ Factory/AbstractFactory.cs
@@ -0,0 +1,42 @@
+﻿namespace Factory
+{
+    public class AbstractFactory
+    {
+        public AbstractFactory()
+        {
+            var android = new DeviceOS(new Android());
+            var appleOS = new DeviceOS(new Apple());
+        }
+    }
+
+    public class DeviceOS 
+    {
+        public Button Button { get; private set; }
+
+        public DeviceOS(IUIFactory uIFactory)
+        {
+            Button = uIFactory.CreateButton();
+        }
+    }
+    
+    public interface IUIFactory
+    {
+        Button CreateButton();
+    }
+
+    public class Apple : IUIFactory
+    {
+        public Button CreateButton()
+        {
+            return new Button() { Type = "This is apple button" };
+        }
+    }
+
+    public class Android : IUIFactory
+    {
+        public Button CreateButton()
+        {
+            return new Button() { Type = "This is android button" };
+        }
+    }
+}
---
+++ Factory/Factory.cs
@@ -0,0 +1,37 @@
+﻿namespace Factory
+{
+
+    public class Dropdown 
+    {
+        public Button Button { get; private set; }
+
+        public Dropdown()
+        {
+            Button = ButtonFactory.CreateButton();
+        }
+    }
+
+    public class NavigationBar
+    {
+        public Button Button { get; private set; }
+
+        public NavigationBar()
+        {
+            Button = ButtonFactory.CreateButton();
+        }
+    }
+
+    public class Button
+    {
+        public string Type { get; set; }
+    }
+
+    public class ButtonFactory
+    {
+        public static Button CreateButton()
+        {
+            return new Button() { Type = "Default button" };
+
+        }
+    }
+}
